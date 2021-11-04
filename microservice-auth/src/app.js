2d8d99cf325a1ce12e710145f82149d324219d93
thiaguinhoo <thiagoluizsog@gmail.com>
2021-11-04 07:57:14 -0300

    add middlewares

--- microservice-auth/src/app.js
+++ microservice-auth/src/app.js
@@ -1,6 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan')
+  app.use(morgan('tiny'))
+}
 
 module.exports = app
--- microservice-auth/src/server.js
+++ microservice-auth/src/server.js
@@ -2,4 +2,6 @@ const app = require('./app')
 
 const serverPort = process.env.SERVER_PORT || 3333
 
-app.listen(serverPort, () => console.log(`Server running on *:${serverPort}`))
+app.listen(
+  serverPort, () => console.log(`Server running on *:${serverPort}`
+))
