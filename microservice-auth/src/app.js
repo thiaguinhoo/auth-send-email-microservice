84bad01aabb61f2f05c6c7376f9861de24152053
thiaguinhoo <thiagoluizsog@gmail.com>
2021-11-04 09:50:53 -0300

    add express-validator

--- microservice-auth/src/app.js
+++ microservice-auth/src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const { body, validationResult } = require('express-validator')
 
 const authenticateMiddleware = require('./middlewares/authenticate')
 
@@ -16,6 +18,16 @@ if (process.env.NODE_ENV === 'development') {
 
 app.get('/protected', authenticateMiddleware, async (req, res) => {
   res.json({ message: 'Welcome to development world' })
+})
+
+app.post('/get-token',
+  body('email').isEmail(),
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.json({ errors: errors.array() })
+    const { email } = req.body
+    const token = jwt.sign({ email }, 'S3cr3t-k3y')
+    res.json({ token })
 })
 
 module.exports = app
